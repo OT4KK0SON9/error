9319a3c3606120dc90bd808bbb96d62d180e75da
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-10 13:34:43 +0700

    Add files via upload

---
+++ DaftarScript.sh
@@ -0,0 +1,16 @@
+figlet Otak Kosong | lolcat
+echo -e “Selamag Menunaikan Ibadah Puasa 11/03/24“ |lolcat
+echo -e “Daftar Script $red : Otak Kosong $white” |lolcat
+echo -e “By : Otak Kosong $white ” |lolcat
+echo “”
+echo -e $b “1. Spam Sms 1x${enda}”;
+echo -e “>” | lolcat
+echo -e $r “2. Spam Wa${endc}”;
+echo -e “>” | lolcat
+echo -e $g “3. Spam Teroos${endc}”;
+echo -e “>” | lolcat
+echo -e $c “4. Play Music${endc}”;
+echo -e “>” | lolcat
+echo Lapor Bug
+echo https://wa.me/6283841852697
+echo “created by : Otak Kosong” | lolcat
