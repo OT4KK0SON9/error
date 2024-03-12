f95ca6caec4381f369dbe7958602febeb3a77269
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-12 15:04:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
 pkg update && pkg upgrade
 pkg install git
 pkg install python3
+pip install colorama bs4 requests pyfiglet tqdm
+pip2 install colorama bs4 requests pyfiglet tqdm
 git clone https://github.com/OT4KK0SON9/error
+clear
+echo "| github = OT4KK0SON9 | youtube = Otak Kosong |"
 cd error
 bash DaftarScript.sh
 ls
